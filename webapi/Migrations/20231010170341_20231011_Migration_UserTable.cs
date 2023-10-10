eb504634409d61ce4e57f6cc5b8314d009a2eb49
helmichehasni <143876217+helmichehasni@users.noreply.github.com>
2023-10-11 01:07:39 +0800

    UserTable changes
    Add 2 new migration

---
+++ webapi/Migrations/20231010170341_20231011_Migration_UserTable.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20231011_Migration_UserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "username",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "first_name",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "username",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "first_name",
+                table: "users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- webapi/Models/Users.cs
+++ webapi/Models/Users.cs
@@ -5,14 +5,14 @@ namespace webapi.Models
 	public class Users
 	{
         public int id { get; set; }
-        public string username { get; set; }
+        public string? username { get; set; }
         public string password { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
+        public string? first_name { get; set; }
+        public string? last_name { get; set; }
         public string? telephone { get; set; }
         public string? mobile { get; set; }
         public string? email { get; set; }
-        public string role { get; set; }
+        public string? role { get; set; }
 		public DateTime? deleted_at { get; set; }
 		public int? deleted_by { get; set; }
 		public DateTime? moodified_at { get; set; }
